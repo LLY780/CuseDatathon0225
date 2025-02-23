a7591eab41b28284409d435bc74387c96ab032da
noobhell304 <39669794+noobhell304@users.noreply.github.com>
2025-02-22 20:34:36 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -37,23 +37,58 @@ def propdensityrad(radius, prop): #radius in miles
 
     return count / area
 
+def proplstrad(radius, prop):
+    rlst = []
+    propcd = (prop.getObjLat(), prop.getObjLong())
+    for i in range(len(parcelTotalVal)):
+        if getDistance(propcd, (properties[i].getObjLat(), properties[i].getObjLong())) < radius:
+            rlst.append(properties[i])
+    return rlst
+
+
 def medianValue(lst):
     middle = len(parcelTotalVal) // 2
     if len(parcelTotalVal) % 2 == 0:
         return (lst[middle]+lst[middle+1])/2
     else:
         return lst[middle]
+    
+def medianValuerad(radius,prop):
+    ulst = proplstrad(radius, prop)
+    middle = len(ulst) // 2
+    if len(ulst) % 2 == 0:
+        return (ulst[middle] + ulst[middle + 1]) / 2
+    else:
+        return ulst[middle]
 
 def meanValue(lst):
-    sum = 0
+    psum = 0
     for i in range(len(parcelTotalVal)):
-        sum += lst[i]
-    return round(sum / len(parcelTotalVal),2)
+        psum += lst[i]
+    return round(psum / len(parcelTotalVal),2)
 
 
-# def standarddev(lst):
-#     topstuff = 
+def meanValuerad(radius,prop):
+    ulst = proplstrad(radius, prop)
+    psum = 0
+    for i in range(len(ulst)):
+        psum += ulst[i]
+    return round(psum / len(ulst),2)
+
+def standarddev(lst):
+    topstuff = 0
+    for i in range(len(parcelTotalVal)):
+        topstuff += (lst[i] - meanValue(lst))**2
+    return sqrt(topstuff/len(parcelTotalVal))
+
+def standarddevrad(radius, prop):
+    ulst = proplstrad(radius, prop)
+    topstuff = 0
+    for i in range(len(ulst)):
+        topstuff += (ulst[i] - meanValue(ulst))**2
+    return sqrt(topstuff/len(ulst))
+
 
 
 print(propdensityrad(1.5,searchProp("940 COMSTOCK AVE & COLVIN ST")))
-print(propdensity())
+print(proplstrad(1.5,searchProp("940 COMSTOCK AVE & COLVIN ST")))
